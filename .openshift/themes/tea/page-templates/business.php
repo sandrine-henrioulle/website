a1ea6bd2cb14d1ff8e3b245a271a12aac13b7342
Schmitz Denis <schmitzdenis@smartseatscorp.com>
2014-10-04 18:15:37 +0200

    add sections template

--- .openshift/themes/tea/page-templates/business.php
+++ .openshift/themes/tea/page-templates/business.php
@@ -1,54 +1,11 @@
-<?php
-/**
- * Template Name: Business Template
- *
- * Displays the Business Template of the theme.
- *
- * @package ThemeGrill
- * @subpackage Spacious
- * @since Spacious 1.0
- */
-?>
+<?php /** * Template Name: Sections Template * * Displays the Section Template of the theme. * * @package ThemeGrill * @subpackage Spacious * @since Spacious 1.0 */ ?>
 
 <?php get_header(); ?>
 
 <div id="content" class="clearfix">
 
-	<?php 
-	if( is_active_sidebar( 'spacious_business_page_top_section_sidebar' ) ) {
-		// Calling the business page top section sidebar if it exists.
-		if ( !dynamic_sidebar( 'spacious_business_page_top_section_sidebar' ) ):
-		endif;
-	}
+    <?php get_template_part( 'content', 'page' ); ?>
 
-	if( is_active_sidebar( 'spacious_business_page_middle_section_left_half_sidebar' ) || is_active_sidebar( 'spacious_business_page_middle_section_right_half_sidebar' )) {
-	?>
-	<!-- <div class="clearfix"> -->
-		<div class="tg-one-half">
-			<?php
-			// Calling the business page middle section left half sidebar if it exists.
-			if ( !dynamic_sidebar( 'spacious_business_page_middle_section_left_half_sidebar' ) ):
-			endif;
-			?>
-		</div>
-		<div class="tg-one-half tg-one-half-last">
-			<?php
-			// Calling the business page middle section right half sidebar if it exists.
-			if ( !dynamic_sidebar( 'spacious_business_page_middle_section_right_half_sidebar' ) ):
-			endif;
-			?>
-		</div>
-	<div class="clearfix"></div>
-	<?php
-	}
-
-	if( is_active_sidebar( 'spacious_business_page_bottom_section_sidebar' ) ) {
-		// Calling the business page bottom section sidebar if it exists.
-		if ( !dynamic_sidebar( 'spacious_business_page_bottom_section_sidebar' ) ):
-		endif;
-	}
-	?>
-
-	</div>
+</div>
 
 <?php get_footer(); ?>

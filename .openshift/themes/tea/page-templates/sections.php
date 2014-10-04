441b2d44bee101c6074947cbe7b5d6a37887d849
Schmitz Denis <schmitzdenis@smartseatscorp.com>
2014-10-04 18:32:27 +0200

    sections fix

--- .openshift/themes/tea/page-templates/sections.php
+++ .openshift/themes/tea/page-templates/sections.php
@@ -1,8 +1,8 @@
-<?php
+<?php 
 /**
  * Template Name: Sections Template
  *
- * Displays the Section Template of the theme.
+ * Displays the Business Template of the theme.
  *
  * @package ThemeGrill
  * @subpackage Spacious
@@ -12,13 +12,29 @@
 
 <?php get_header(); ?>
 
-<div id="content" class="clearfix">
+	<?php do_action( 'spacious_before_body_content' ); ?>
 
+	<div id="primary">
+		<div id="content" class="clearfix">
+			<?php while ( have_posts() ) : the_post(); ?>
 
-	<?php get_template_part( 'content', 'page' ); ?>
-    
-     <?php the_content(); ?>
+				<?php get_template_part( 'content', 'page' ); ?>
 
-</div>
+				<?php
+					do_action( 'spacious_before_comments_template' );
+					// If comments are open or we have at least one comment, load up the comment template
+					if ( comments_open() || '0' != get_comments_number() )
+						comments_template();					
+	      		do_action ( 'spacious_after_comments_template' );
+				?>
+
+			<?php endwhile; ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+	
+	<?php spacious_sidebar_select(); ?>
+
+	<?php do_action( 'spacious_after_body_content' ); ?>
 
 <?php get_footer(); ?>

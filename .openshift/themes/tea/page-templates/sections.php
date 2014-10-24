613e0990c58de2dc7aef0c7fc0406cda15483792
Schmitz Denis <schmitzdenis@smartseatscorp.com>
2014-10-24 23:04:40 +0200

    mod widget service RC2

--- .openshift/themes/tea/page-templates/sections.php
+++ .openshift/themes/tea/page-templates/sections.php
@@ -28,19 +28,9 @@
                         $my_wp_query = new WP_Query();
                         $all_wp_pages = $my_wp_query->query(array('post_type' => 'page', 'post_parent' =>  $parentPageId,'post_status' => 'publish','orderby' => 'menu_order', 'order' => 'ASC'));
 
-                        // Get the page as an Object
-                        //$portfolio =  get_page_by_title('Services');
-
-                        // Filter through all pages and find Portfolio's children
-                        //$portfolio_children = get_page_children( $parentPageId, $all_wp_pages );
-                        //var_dump($portfolio_children);
-                        //var_dump($all_wp_pages);
-                        //$portfolio->ID
-                        $prevPostId = 0;
-                        foreach($all_wp_pages as $child_page){
-                          //if($child_page->post_parent == $parentPageId){
+                        foreach($all_wp_pages as $index=>$child_page){
                           ?>
-                            <div class="section <?php echo strtolower($child_page->post_title)?>" name="<?php echo 'section_'.$prevPostId;?>">
+                            <div class="section <?php echo strtolower($child_page->post_title)?>" name="<?php echo 'section_'.$all_wp_pages[$index+1]->ID;?>">
                                 <div class="section-text">
                                     <?php 
                                     echo $child_page->post_content; 
@@ -48,11 +38,8 @@
                                 </div>
                             </div>
                         <?php                     
-                        //}
-                          $prevPostId = $child_page->ID;
                         }
-                            // echo what we get back from WP to the browser
-                          //  echo '<pre>' . print_r( $portfolio_children, true ) . '</pre>';
+
                         ?>
 			<?php endwhile; ?>
 

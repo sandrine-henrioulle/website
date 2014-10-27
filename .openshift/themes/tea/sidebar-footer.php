9f49644b862a8e88c04585958342778e683b9699
Schmitz Denis <schmitzdenis@smartseatscorp.com>
2014-10-27 21:22:31 +0100

    remove not needed footer-sidebar html

--- .openshift/themes/tea/sidebar-footer.php
+++ .openshift/themes/tea/sidebar-footer.php
@@ -25,34 +25,11 @@ if( !is_active_sidebar( 'spacious_footer_sidebar_one' ) &&
 <div class="footer-widgets-wrapper">
 	<div class="inner-wrap">
 		<div class="footer-widgets-area clearfix">
-			<div class="tg-one-fourth tg-column-1">
 				<?php
 				// Calling the footer sidebar if it exists.
 				if ( !dynamic_sidebar( 'spacious_footer_sidebar_one' ) ):
 				endif;
 				?>
-			</div>
-			<div class="tg-one-fourth tg-column-2">
-				<?php
-				// Calling the footer sidebar if it exists.
-				if ( !dynamic_sidebar( 'spacious_footer_sidebar_two' ) ):
-				endif;
-				?>
-			</div>
-			<div class="tg-one-fourth tg-after-two-blocks-clearfix tg-column-3">
-				<?php
-				// Calling the footer sidebar if it exists.
-				if ( !dynamic_sidebar( 'spacious_footer_sidebar_three' ) ):
-				endif;
-				?>
-			</div>
-			<div class="tg-one-fourth tg-one-fourth-last tg-column-4">
-				<?php
-				// Calling the footer sidebar if it exists.
-				if ( !dynamic_sidebar( 'spacious_footer_sidebar_four' ) ):
-				endif;
-				?>
-			</div>
 		</div>
 	</div>
 </div>
